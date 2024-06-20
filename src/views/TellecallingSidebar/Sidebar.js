08b67fd066734ebb86179c969e6c3029c80f3246
ashwakshaikh786 <85924211+ashwakshaikh786@users.noreply.github.com>
2024-06-20 16:45:29 +0530

    Revert "CHNAGES"
    This reverts commit 73d9a30541f8cd55a085e15254f397ab8139f2db, reversing
    changes made to 3b7ac2c5b3f0aabadadf2994354f0cee31ebb915.

--- src/views/TellecallingSidebar/Sidebar.js
+++ src/views/TellecallingSidebar/Sidebar.js
@@ -12,13 +12,29 @@ import {
   InputAdornment,
   ListItemAvatar,
   IconButton,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+  Menu,
+  ListItemIcon,
+  Popover,
 } from "@mui/material";
 import axios from "axios";
 import PersonIcon from "@mui/icons-material/Person";
-import TablePagination from "@mui/material/TablePagination";
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { Divider } from "@mui/material";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import GetAppIcon from "@mui/icons-material/GetApp";
+import SortIcon from "@mui/icons-material/Sort";
 
-const Sidebar = () => {
+const Sidebar = ({ onEdit, onItemClick, onCreate  }) => {
   const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -26,14 +42,15 @@ const Sidebar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
+  const [anchorElFilter, setAnchorElFilter] = useState(null);
+  const [anchorElDots, setAnchorElDots] = useState(null);
+  const [sortOption, setSortOption] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
-
-  useEffect(() => {
-    setFilteredRows(rows);
-  }, [rows]);
 
   const fetchData = async () => {
     try {
@@ -51,6 +68,10 @@ const Sidebar = () => {
   };
 
   useEffect(() => {
+    setFilteredRows(rows);
+  }, [rows]);
+
+  useEffect(() => {
     const lowerCaseQuery = searchQuery.toLowerCase().trim();
     if (lowerCaseQuery === "") {
       setFilteredRows(rows);
@@ -62,7 +83,7 @@ const Sidebar = () => {
       );
       setFilteredRows(filteredData);
     }
-    setPage(0); // Reset page to first page when search query changes
+    setPage(0);
   }, [searchQuery, rows]);
 
   const handleSearchChange = (event) => {
@@ -80,43 +101,221 @@ const Sidebar = () => {
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0); // Reset to first page when rows per page changes
+    setPage(0);
   };
 
+  const handleDelete = async () => {
+    try {
+      const response = await axios.post(
+        "https://ideacafe-backend.vercel.app/api/proxy/api-delete-telecalling.php",
+        {
+          Tid: deleteId,
+          DeleteUID: 1,
+        }
+      );
+      if (response.data.status === "Success") {
+        setRows(rows.filter((row) => row.Tid !== deleteId));
+        console.log("Deleted successfully");
+        setConfirmDelete(false);
+      }
+    } catch (error) {
+      console.error("Error deleting data:", error);
+      setError(error);
+    }
+  };
+
+  const handleOpenConfirmDelete = (id) => {
+    setDeleteId(id);
+    setConfirmDelete(true);
+  };
+  
+    const handleListItemClick = (item) => {
+    onItemClick(item);
+  };
+
+  const handleCloseConfirmDelete = () => {
+    setConfirmDelete(false);
+    setDeleteId(null);
+  };
+
+  const handleFilterMenuOpen = (event) => {
+    setAnchorElFilter(event.currentTarget);
+  };
+
+  const handleFilterMenuClose = () => {
+    setAnchorElFilter(null);
+  };
+
+  const handleDotsMenuOpen = (event) => {
+    setAnchorElDots(event.currentTarget);
+  };
+
+  const handleDotsMenuClose = () => {
+    setAnchorElDots(null);
+  };
+
+  const handleSortOptionChange = (option) => {
+    setSortOption(option);
+    setAnchorElFilter(null);
+    sortData(option);
+  };
+
+  const sortData = (option) => {
+    const sortedRows = [...filteredRows];
+    switch (option) {
+      case "asc":
+        sortedRows.sort(
+          (a, b) =>
+            new Date(a.NextFollowUpDate) - new Date(b.NextFollowUpDate)
+        );
+        break;
+      case "desc":
+        sortedRows.sort(
+          (a, b) =>
+            new Date(b.NextFollowUpDate) - new Date(a.NextFollowUpDate)
+        );
+        break;
+      case "a-z":
+        sortedRows.sort((a, b) => a.PartyName.localeCompare(b.PartyName));
+        break;
+      case "z-a":
+        sortedRows.sort((a, b) => b.PartyName.localeCompare(a.PartyName));
+        break;
+      default:
+        break;
+    }
+    setFilteredRows(sortedRows);
+  };
+
+  const jsonToCSV = (json) => {
+    const header = Object.keys(json[0]).join(",");
+    const values = json
+      .map((obj) => Object.values(obj).join(","))
+      .join("\n");
+    return `${header}\n${values}`;
+  };
+
+  const handleDownload = () => {
+    const csv = jsonToCSV(rows);
+    const blob = new Blob([csv], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "Telecalling.csv";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
+
+
   return (
-    <Card sx={{ width: 400, padding: 5, height: "90vh" }}>
-      <Grid item xs={12} sx={{ marginTop: 4.8, marginBottom: 3 }}>
-        <Box>
-          <Typography
-            variant="body2"
-            sx={{ marginTop: 5, fontWeight: "bold", fontSize: 20 }}
-          >
+    <Card
+      sx={{
+        width: 330,
+        padding: 5,
+        height: "auto",
+        minHeight: 800,
+        overflowY: "auto",
+      }}
+    >
+      <Grid item xs={12} sx={{ marginBottom: 3 }}>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="body2" sx={{ fontWeight: "bold", fontSize: 20 }}>
             All Telecaller
           </Typography>
+          <Box display="flex" alignItems="center">
+            <IconButton
+              aria-label="filter"
+              sx={{ color: "grey" }}
+              onClick={handleFilterMenuOpen}
+            >
+              <SortIcon />
+            </IconButton>
+            <Popover
+              id="sort-popover"
+              open={Boolean(anchorElFilter)}
+              anchorEl={anchorElFilter}
+              onClose={handleFilterMenuClose}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <MenuItem onClick={() => handleSortOptionChange("asc")}>
+                Date Asc
+              </MenuItem>
+              <MenuItem onClick={() => handleSortOptionChange("desc")}>
+                Date Desc
+              </MenuItem>
+              <MenuItem onClick={() => handleSortOptionChange("a-z")}>
+                Name A-Z
+              </MenuItem>
+              <MenuItem onClick={() => handleSortOptionChange("z-a")}>
+                Name Z-A
+              </MenuItem>
+            </Popover>
+
+            <IconButton
+              aria-label="more"
+              aria-controls="menu"
+              aria-haspopup="true"
+              onClick={handleDotsMenuOpen}
+              sx={{ color: "grey" }}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="menu"
+              anchorEl={anchorElDots}
+              open={Boolean(anchorElDots)}
+              onClose={handleDotsMenuClose}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <MenuItem onClick={handleDownload}>
+                <ListItemIcon>
+                  <GetAppIcon fontSize="small" />
+                </ListItemIcon>
+                Download All Data
+              </MenuItem>
+            </Menu>
+          </Box>
         </Box>
       </Grid>
-      {/* Search Input */}
+
       <TextField
-        size="small"
-        placeholder="Search"
+        label="Search"
+        variant="outlined"
+        fullWidth
         value={searchQuery}
         onChange={handleSearchChange}
         InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <PersonIcon />
-            </InputAdornment>
-          ),
           endAdornment: (
             <InputAdornment position="end">
-              {searchQuery && (
-                <IconButton onClick={handleClearSearch} size="small">
-                  &#x2715;
-                </IconButton>
-              )}
+              <IconButton
+                aria-label="clear search"
+                onClick={handleClearSearch}
+                edge="end"
+                sx={{ color: "grey" }}
+              >
+                <PersonIcon />
+              </IconButton>
             </InputAdornment>
           ),
-          sx: {
+        }}
+        sx={{
+          "& .MuiOutlinedInput-root": {
             borderRadius: "20px",
             "& fieldset": {
               borderRadius: "20px",
@@ -124,6 +323,7 @@ const Sidebar = () => {
           },
         }}
       />
+
       {filteredRows.length === 0 ? (
         <Box
           sx={{
@@ -137,6 +337,15 @@ const Sidebar = () => {
         >
           <ErrorOutlineIcon sx={{ fontSize: 40, mb: 2 }} />
           <Typography variant="h6">No data found</Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+            onClick={onCreate}
+            sx={{ mt: 2 }}
+          >
+            Create Telecalling
+          </Button>
         </Box>
       ) : (
         <>
@@ -144,37 +353,88 @@ const Sidebar = () => {
             {filteredRows
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((item) => (
-                <ListItem key={item.telecallingID} disablePadding>
-                  <ListItemAvatar>
-                    <Avatar sx={{ bgcolor: "#2563EB" }}>
-                      <img src="/ManIcon.svg" alt="Man Icon" style={{ width: 24, height: 24 }} />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={item.PartyName}
-                    secondary={
-                      <Typography variant="body2">{item.Mobile}</Typography>
-                    }
-                    primaryTypographyProps={{ variant: "subtitle1" }}
-                    secondaryTypographyProps={{ variant: "body2" }}
-                  />
-                </ListItem>
+                <React.Fragment key={item.Tid}>
+                  <Card sx={{marginBottom:2}}>
+                  <ListItem disablePadding onClick={() => handleListItemClick(item)}>
+  <ListItemAvatar>
+    <Avatar
+      alt="John Doe"
+      sx={{ width: 40, height: 40, margin: 2 }}
+      src="/images/avatars/1.png"
+    />
+  </ListItemAvatar>
+  <ListItemText
+    primary={
+      <Typography
+        variant="subtitle1"
+        style={{ fontWeight: "bold" }}
+      >
+        {item.PartyName}
+      </Typography>
+    }
+    secondary={
+      <>
+        <Typography variant="body2" style={{ fontSize: 10 }}>
+          Phone: {item.Mobile}
+        </Typography>
+        <Typography variant="body2" style={{ fontSize: 10 }}>
+          Next follow Up: {item.NextFollowUpDate}
+        </Typography>
+      </>
+    }
+    secondaryTypographyProps={{ variant: "body2" }}
+  />
+  <Box
+    display="flex"
+    flexDirection="column"
+    alignItems="flex-end"
+  >
+    <IconButton
+      aria-label="edit"
+      onClick={(event) => {
+        event.stopPropagation();
+        onEdit(item);
+      }}
+      sx={{ color: "blue" }}
+    >
+      <EditIcon />
+    </IconButton>
+    <IconButton
+      aria-label="delete"
+      onClick={(event) => {
+        event.stopPropagation();
+        handleOpenConfirmDelete(item.Tid);
+      }}
+      sx={{ color: "red" }}
+    >
+      <DeleteIcon />
+    </IconButton>
+  </Box>
+</ListItem>
+
+                  </Card>
+                </React.Fragment>
               ))}
           </List>
-          <Box sx={{ ml: -3, mt: 2 }}>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25]}
-              component="div"
-              count={filteredRows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              sx={{ fontSize: "0.75rem" }}
-            />
-          </Box>
         </>
       )}
+
+      <Dialog open={confirmDelete} onClose={handleCloseConfirmDelete}>
+        <DialogTitle>{"Confirm Delete"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Are you sure you want to delete this record?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseConfirmDelete} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDelete} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Card>
   );
 };
