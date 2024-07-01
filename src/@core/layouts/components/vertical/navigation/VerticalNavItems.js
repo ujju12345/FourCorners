c5f37fd3c15695125241f8f5e267defa5716b62f
ashwakshaikh786 <85924211+ashwakshaikh786@users.noreply.github.com>
2024-07-01 14:25:15 +0530

    sidebar code

--- src/@core/layouts/components/vertical/navigation/VerticalNavItems.js
+++ src/@core/layouts/components/vertical/navigation/VerticalNavItems.js
@@ -1,22 +1,22 @@
+// verticle nav items
+
 // ** Custom Menu Components
+import VerticalNavDropdown from './SidebarDropdown';
 import VerticalNavLink from './VerticalNavLink';
 import VerticalNavSectionTitle from './VerticalNavSectionTitle';
 
 // Function to determine the component based on item type
 const resolveNavItemComponent = (item) => {
   if (item.sectionTitle) return VerticalNavSectionTitle;
+  if (item.children) return VerticalNavDropdown; // Handle dropdown items
   return VerticalNavLink;
 };
 
 const VerticalNavItems = (props) => {
-  // Destructure props to get verticalNavItems
   const { verticalNavItems } = props;
 
-  // Check if verticalNavItems is an array and map over it
   const RenderMenuItems = (verticalNavItems || []).map((item, index) => {
-    const TagName = resolveNavItemComponent(item); // Determine the component to render
-
-    // Render the component with props and key
+    const TagName = resolveNavItemComponent(item);
     return <TagName {...props} key={index} item={item} />;
   });
 
