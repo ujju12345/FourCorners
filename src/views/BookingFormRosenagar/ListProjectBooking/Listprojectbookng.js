d80c836bc740851dc2b7b31ee26b93084e6ef012
ashwakshaikh786 <85924211+ashwakshaikh786@users.noreply.github.com>
2024-08-27 12:48:12 +0530

    changes

--- src/views/BookingFormRosenagar/ListProjectBooking/Listprojectbookng.js
+++ src/views/BookingFormRosenagar/ListProjectBooking/Listprojectbookng.js
@@ -834,7 +834,7 @@ const Listprojectbookng = ({ onChequeReceiptClick, item }) => {
                       key={option.BookingremarkID}
                       value={option.BookingremarkID}
                     >
-                      {option.RemarkName}   Date: {option.RemarkDate}/{option.AmountTypeName}
+                      {option.RemarkName}   `({option.RemarkDate},{option.AmountTypeName})
                     </MenuItem>
                   ))}
                 </TextField>
